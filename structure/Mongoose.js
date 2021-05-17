7058b7e7908aa1764f061cfb2f0e3c412c17a351
Pronicio <52421988+Pronicio@users.noreply.github.com>
2021-05-17 18:56:36 +0200

    🎃 Ajout du fichier Mongoose.js

--- structure/Mongoose.js
+++ structure/Mongoose.js
@@ -1,1 +1,53 @@
-//Hello World
+const mongoose = require("mongoose");
+const { mongoKey } = require("../configs/config.json")
+const { Guild } = require("../models/index")
+
+module.exports = {
+    init: () => {
+        const mongOptions = { useNewUrlParser: true, useUnifiedTopology: true }
+
+        mongoose.connect(mongoKey, mongOptions).then(() => {
+            console.log("• Mongoose connecté !")
+        }).catch((err) => {
+            console.log("• Error Mongoose ! "+err)
+        });
+    }
+}
+
+module.exports.createGuild = async function (guildID){
+    guildDB = new Guild({
+      _id: mongoose.Types.ObjectId(),
+      id: guildID
+    })
+
+    await guildDB.save();
+    return true;
+};
+
+module.exports.getGuild = async function (guildID){
+
+    let guildDB = await Guild.findOne( { id: guildID } );
+  
+    if(guildDB){
+      return guildDB;
+    } else {
+      guildDB = new Guild({
+        _id: mongoose.Types.ObjectId(),
+        id: guildID
+      })
+      await guildDB.save();
+      return guildDB;
+    }
+};
+
+module.exports.deleteGuild = async function (guildID){
+
+  let guildDB = await Guild.findOne( { id: guildID } );
+
+  if(guildDB){
+    await Guild.deleteOne({ id: guildID });
+    return true;
+  } else {
+    return false;
+  }
+};
